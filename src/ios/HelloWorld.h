6c37d9ce95b4b04b1bc2485f20a4b0563f1ab302
Srinidhi Anand Rao <srinidhi.rao@unvired.io>
2019-06-19 18:33:43 +0530

    remove unrelated files

--- src/ios/HelloWorld.h
+++
@@ -1,9 +0,0 @@
-/********* HelloWorld.m Cordova Plugin Implementation *******/
-
-#import <Cordova/CDV.h>
-
-@interface HelloWorld : CDVPlugin {
-  // Member variables go here.
-}
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
-@end
